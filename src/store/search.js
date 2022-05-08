7a651a9a2f0b237a5d67b2f89df29ee74f16ce80
Lucas Wang <474.lucas.wang@gmail.com>
2022-05-08 23:58:30 +0800

    Add vuex search module.

---
+++ src/store/search.js
@@ -0,0 +1,36 @@
+import {reqSearchResults} from '@/api';
+
+
+const state = {
+    searchResults: [],
+};
+
+const mutations = {
+    SET_SEARCH_RESULTS(state, searchResults) {
+        state.searchResults = searchResults;
+    }
+};
+
+const SUCCESS_CODE = 200;
+
+const actions = {
+    async getSearchResults({commit}, searchParams) {
+        console.log('getSearchResults, searchParams: ', searchParams);
+
+        const response = await reqSearchResults(searchParams);
+        if (response.code === SUCCESS_CODE) {
+            commit('SET_SEARCH_RESULTS', response.data);
+        } else {
+            console.log('获取搜索结果失败，状态码：' + response.code);
+            return new Promise(() => undefined);
+        }
+    }
+};
+
+
+export default {
+    // namespace: true,
+    state,
+    mutations,
+    actions,
+};
