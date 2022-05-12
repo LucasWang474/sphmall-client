6118848d748387acba77e9e6bb0591452bd70901
Lucas Wang <474.lucas.wang@gmail.com>
2022-05-13 01:05:06 +0800

    添加 toggleIsChecked

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,4 +1,4 @@
-import {reqCartList, reqUpdateCart} from '@/api';
+import {reqCartList, reqChangeChecked, reqUpdateCart} from '@/api';
 
 const state = {
     cartList: [],
@@ -28,6 +28,18 @@ const actions = {
             dispatch('updateCartList');
         } else {
             return Promise.reject('添加购物车失败' + response.msg);
+        }
+    },
+
+    async toggleIsChecked({dispatch, state}, productId) {
+        const target = state.cartList.find(item => item.skuId === productId);
+        target.isChecked = +target.isChecked ? 0 : 1;
+
+        const response = await reqChangeChecked(productId, target.isChecked);
+        if (response.code === SUCCESS_CODE) {
+            dispatch('updateCartList');
+        } else {
+            return Promise.reject('更新商品选中失败' + response.msg);
         }
     }
 };
