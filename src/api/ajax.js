85d86d6e072377eff41cdb8fa1c45df966faa1b7
Lucas Wang <474.lucas.wang@gmail.com>
2022-05-15 00:11:21 +0800

    Init config.headers.token to undefined

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -9,13 +9,13 @@ const ajax = axios.create({
     baseURL: 'http://gmall-h5-api.atguigu.cn',
     timeout: 5000,
     headers: {
-        userTempId: getUserTempId()
+        userTempId: getUserTempId(),
+        token: localStorage.getItem('token') || undefined,
     }
 });
 
 ajax.interceptors.request.use(
     config => {
-        config.headers.token = localStorage.getItem('token') || undefined;
         NProgress.start();
         return config;
     }
